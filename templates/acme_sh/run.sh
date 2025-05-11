630438cb8bf088bfa4f4bde1379e5874bf086b3c
zzq <i@zhangzqs.cn>
2025-05-12 00:15:19 +0800

    acme自动签证书

---
+++ templates/acme_sh/run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+{%- for key, value in service_vars.conf.environments.items() %}
+export {{ key }}='{{ value }}'
+{%- endfor %}
+
+acme.sh --register-account -m '{{ service_vars.conf.email }}'
+acme.sh --issue --dns '{{ service_vars.conf.dns_provider }}' -d '*.{{ home_base_domain }}'    
+acme.sh --install-cert -d '*.{{ home_base_domain }}'
+    --key-file '/sslcerts/{{ home_base_domain }}.key' \
+    --fullchain-file '/sslcerts/{{ home_base_domain }}.crt' 
