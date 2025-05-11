171def0c774273d2a25790dd3de00f61ab5c033f
zzq <i@zhangzqs.cn>
2025-05-12 01:21:52 +0800

    添加一些脚本

--- templates/acme_sh/run.sh
+++ templates/acme_sh/run.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
+set -e
 
 {%- for key, value in service_vars.conf.environments.items() %}
 export {{ key }}='{{ value }}'
 {%- endfor %}
 
 acme.sh --register-account -m '{{ service_vars.conf.email }}'
-acme.sh --issue --dns '{{ service_vars.conf.dns_provider }}' -d '*.{{ home_base_domain }}'    
+
+acme.sh --issue --dns '{{ service_vars.conf.dns_provider }}' -d '*.{{ home_base_domain }}' --log 
+
 acme.sh --install-cert -d '*.{{ home_base_domain }}' \
     --key-file '/sslcerts/{{ home_base_domain }}.key' \
     --fullchain-file '/sslcerts/{{ home_base_domain }}.crt' 
