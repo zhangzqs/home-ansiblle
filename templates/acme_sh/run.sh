5924e54b551d06ec670ae30956f50aa6e2f8ae6e
zzq <i@zhangzqs.cn>
2025-05-12 00:29:48 +0800

    修复acme脚本

--- templates/acme_sh/run.sh
+++ templates/acme_sh/run.sh
@@ -6,6 +6,6 @@ export {{ key }}='{{ value }}'
 
 acme.sh --register-account -m '{{ service_vars.conf.email }}'
 acme.sh --issue --dns '{{ service_vars.conf.dns_provider }}' -d '*.{{ home_base_domain }}'    
-acme.sh --install-cert -d '*.{{ home_base_domain }}'
+acme.sh --install-cert -d '*.{{ home_base_domain }}' \
     --key-file '/sslcerts/{{ home_base_domain }}.key' \
     --fullchain-file '/sslcerts/{{ home_base_domain }}.crt' 
