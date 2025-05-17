b8a53615821c149223d7c931f1cd96b7f080a957
zzq <i@zhangzqs.cn>
2025-05-17 19:09:10 +0800

    自动化申请证书

--- templates/acme_sh/run.sh
+++ templates/acme_sh/run.sh
@@ -5,10 +5,13 @@ set -e
 export {{ key }}='{{ value }}'
 {%- endfor %}
 
+echo "注册账号：{{ service_vars.conf.email }}"
 acme.sh --register-account -m '{{ service_vars.conf.email }}'
 
-acme.sh --issue --dns '{{ service_vars.conf.dns_provider }}' -d '*.{{ home_base_domain }}' --log 
+echo "申请域名证书：*.{{ home_base_domain }}, DNS供应商：{{ service_vars.conf.dns_provider }}"
+acme.sh --issue --dns '{{ service_vars.conf.dns_provider }}' -d '*.{{ home_base_domain }}' --log || true
 
+echo "安装证书：*.{{ home_base_domain }}, 证书路径：/sslcerts/{{ home_base_domain }}.crt, 私钥路径：/sslcerts/{{ home_base_domain }}.key"
 acme.sh --install-cert -d '*.{{ home_base_domain }}' \
     --key-file '/sslcerts/{{ home_base_domain }}.key' \
     --fullchain-file '/sslcerts/{{ home_base_domain }}.crt' 
