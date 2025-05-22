c25996f02736af7b862823e04fb06aa392fdb487
zzq <i@zhangzqs.cn>
2025-05-22 15:10:59 +0800

    Revert "重构所有服务配置"
    This reverts commit 2a1a2552bfb8e5183d7121c846b7cfa72fe78aec.

--- templates/acme_sh/run.sh
+++ templates/acme_sh/run.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 set -e
 
-{%- for key, value in acme_sh.conf.environments.items() %}
+{%- for key, value in service_vars.conf.environments.items() %}
 export {{ key }}='{{ value }}'
 {%- endfor %}
 
-echo "注册账号：{{ acme_sh.conf.email }}"
-acme.sh --register-account -m '{{ acme_sh.conf.email }}'
+echo "注册账号：{{ service_vars.conf.email }}"
+acme.sh --register-account -m '{{ service_vars.conf.email }}'
 
-echo "申请域名证书：*.{{ home_base_domain }}, DNS供应商：{{ acme_sh.conf.dns_provider }}"
-acme.sh --issue --dns '{{ acme_sh.conf.dns_provider }}' -d '*.{{ home_base_domain }}' --log || true
+echo "申请域名证书：*.{{ home_base_domain }}, DNS供应商：{{ service_vars.conf.dns_provider }}"
+acme.sh --issue --dns '{{ service_vars.conf.dns_provider }}' -d '*.{{ home_base_domain }}' --log || true
 
 echo "安装证书：*.{{ home_base_domain }}, 证书路径：/sslcerts/{{ home_base_domain }}.crt, 私钥路径：/sslcerts/{{ home_base_domain }}.key"
 acme.sh --install-cert -d '*.{{ home_base_domain }}' \
